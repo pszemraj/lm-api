aa320a5697e4adbea4a0448c581c28d43c1bd68f
Peter <74869040+pszemraj@users.noreply.github.com>
2022-06-13 12:09:17 +0200

    :construction: fix save location when passed path

--- utils.py
+++ utils.py
@@ -51,6 +51,8 @@ def append_entry_outtxt(
         out_path = (
             cwd / "out" / f"api_queries_{model_name}_{get_timestamp()}.{file_extension}"
         )
+    else:
+        out_path = Path(out_path) / f"api_queries_{model_name}_{get_timestamp()}.{file_extension}"
     with open(out_path, "a", encoding="utf-8", errors="ignore") as f:
         f.write(f" ## {prompt}\n")
         f.write(f"Response: \n{response}\n")
